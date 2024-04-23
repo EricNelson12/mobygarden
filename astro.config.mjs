3923e1c508cf2d4829af002864fbce6e3c988896
eric <ericjnelson012@gmail.com>
2024-04-22 23:09:17 -0700

    Update site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://enelson12.github.io',
-	base: 'mobygarden',
+	site: 'https://mobygarden.ca',
 	integrations: [mdx(), sitemap()],
 });
